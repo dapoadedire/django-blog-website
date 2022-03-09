fbc6ab4ebb6f9442a47b30cc15c4e0d6118a8b6b
dapoadedire <adedireadedapo19@gmail.com>
2022-03-09 23:30:09 +0100

    initial commit

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import BlogListView, BlogDetailView #new
+from .views import BlogListView, BlogDetailView,# BlogCreateView#new
 
 urlpatterns = [
     path('', BlogListView.as_view(), name='home'),
     path('post/<int:pk>/', BlogDetailView.as_view(), name='post_detail'),
+#    path('post/new/', BlogCreateView.as_view(), name='post_new')
 ]
--- blog_project/settings.py
+++ blog_project/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-2wjo30ev08fi&f8s&%+nig@#h37uck%!)ky(jffzj)=ardmzg7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
